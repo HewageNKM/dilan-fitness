4ab1ce9fe0e022c3aff0367e5f8654bc75df4f17
Nadun Kawishika <hnkmalwenna@gmail.com>
2024-06-07 14:51:54 +0530

    Hero and Nav complete

---
+++ components/Hero.tsx
@@ -0,0 +1,18 @@
+// @flow
+import * as React from 'react';
+
+type Props = {
+
+};
+export const Hero = () => {
+    return (
+        <div className="w-full mt-20 bg-slate-200 rounded-lg">
+            <div className="flex flex-col pt-20 justify-center items-center mx-auto">
+                <div className="font-light text-xl tracking-tight">
+                    Archive Your Fitness Goals
+                </div>
+                <div className="font-bold uppercase text-[12vw]">Find Your <br/> Strength</div>
+            </div>
+        </div>
+    );
+};
